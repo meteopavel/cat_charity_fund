9b6d149d02dbb1c023d6680f53f60e2cd08d3b80
meteopavel <isu@meteopavel.ru>
2024-04-19 15:00:21 +0800

    Fix models

---
+++ app/models/charity_project.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Text
+
+from app.models.base import Investment
+
+
+class CharityProject(Investment):
+    name = Column(
+        String(100),
+        unique=True,
+        nullable=False,
+    )
+    description = Column(Text, nullable=False)
+
+    def __repr__(self):
+        return (f'{super().__repr__()},'
+                f' Имя: {self.name}, Описание: {self.description}')
